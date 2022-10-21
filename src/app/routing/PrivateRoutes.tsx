313157175c460abc4bb26e4b6d4b495a7da58ade
Philip Ladzaka <philipkelly407@gmail.com>
2022-10-21 17:14:49 +0000

    Update PrivateRoutes.tsx

--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -20,7 +20,7 @@ const PrivateRoutes = () => {
   return (
     <Routes>
       <Route element={<MasterLayout />}>
-        {/* Redirect to Dashboard after success login/registartion */}
+       
         <Route path='auth/*' element={<Navigate to='/dashboard' />} />
         {/* Pages */}
         <Route path='dashboard' element={<DashboardWrapper />} />
@@ -76,7 +76,7 @@ const PrivateRoutes = () => {
             </SuspensedView>
           }
         />
-        {/* Page Not Found */}
+      
         <Route path='*' element={<Navigate to='/error/404' />} />
       </Route>
     </Routes>
